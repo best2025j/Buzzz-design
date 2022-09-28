c0d407117eb3e8dba5f68228cb6f907293979b0d
best <gt@best.com>
2022-09-28 17:51:06 +0100

    form added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import img from "../Assets/image1.png";
-import img1 from "../Assets/art1.png";
+import img1 from "../Assets/art2.png";
 import img2 from "../Assets/ct2.png";
 import img3 from "../Assets/oR.png";
 
@@ -12,24 +12,43 @@ const Login = () => {
           <img src={img} alt="w-full h-full" width={477} height={450} />
         </div>
         {/*  */}
-        <div className="w-1/2">
-          <img src={img1} alt="" />
-          <img src={img2} alt="" />
-          {/* FORM */}
+        <div className="w-1/2 flex flex-col justify-center mx-auto items-center space-y-6">
+          <img src={img1} className="w" alt="" />
+          <img src={img2} className="" alt="" />
           <div>
-            <form action="">
+            <form action="" className="space-y-5">
               <div>
                 <label htmlFor="">Email</label>
-                <input type="text" />
+                <input
+                  className="w-full p-3 rounded-md outline-none border"
+                  placeholder="mail@abc.com"
+                  type="text"
+                />
               </div>
               <div>
                 <label htmlFor="">Password</label>
-                <input type="text" />
+                <input
+                  className="w-full p-3 rounded-md outline-none border"
+                  placeholder="**********"
+                  type="password"
+                />
               </div>
+              <label>
+                <input
+                  className="p-3 bg-[#00B380] rounded-md outline-none "
+                  type="checkbox"
+                />
+                <span className="text-sm"> Remember Me</span>
+                <span className="text-sm float-right text-[#00b380]">
+                  Forgot Password?
+                </span>
+              </label>
               <div>
-                <button>Login</button>
+                <button className="w-full bg-[#00B380] p-3 text-white rounded-md">
+                  Login
+                </button>
               </div>
-              <img src={img3} alt="" className="" width={200} />
+                <img src={img3} alt="" className="mx-auto" />
             </form>
           </div>
         </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import img from "../Assets/image2.png";
+import img1 from "../Assets/art1.png";
+import img2 from "../Assets/ct1.png";
+
 
 const Signup = () => {
   return (
@@ -8,8 +11,57 @@ const Signup = () => {
         <div className="bg-[#00B380] w-1/2 h-screen mx-auto justify-center flex items-center">
           <img src={img} alt="w-full h-full" width={437} height={450} />
         </div>
-        <div className="w-1/2">
-          <div></div>
+        {/*  */}
+        <div className="w-1/2 flex flex-col justify-center mx-auto items-center space-y-6">
+          <img src={img1} className="w" alt="" />
+          <img src={img2} className="" alt="" />
+          <div>
+            <form action="" className="space-y-5">
+              <div>
+                <label htmlFor="">Your name or brand</label>
+                <input
+                  className="w-full p-3 rounded-md outline-none border"
+                  placeholder="create buzz"
+                  type="text"
+                />
+              </div>
+              <div>
+                <label htmlFor="">Email</label>
+                <input
+                  className="w-full p-3 rounded-md outline-none border"
+                  placeholder="mail@abc.com"
+                  type="text"
+                />
+              </div>
+              <div>
+                <label htmlFor="">Password</label>
+                <input
+                  className="w-full p-3 rounded-md outline-none border"
+                  placeholder="**********"
+                  type="password"
+                />
+              </div>{" "}
+              <div>
+                <label htmlFor="">Confrim Password</label>
+                <input
+                  className="w-full p-3 rounded-md outline-none border"
+                  placeholder="**********"
+                  type="password"
+                />
+                <p className="text-sm">
+                  Create an account you have agreed to our{" "}
+                  <span className="text-[#00B380]">terms </span>
+                  and
+                  <span className="text-[#00B380]"> condition</span>
+                </p>
+              </div>
+              <div>
+                <button className="w-full bg-[#00B380] p-3 text-white rounded-md">
+                  Create Account
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
